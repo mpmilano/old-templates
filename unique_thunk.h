c09440b5327374ad5c307b3385b645fdb16bff1c
Matthew Milano <mpmilano@cs.brown.edu>
2012-07-29 16:36:56 -0400

    renamed unique_threaded_thunk to unique_thunk, using async model to determine threading

--- unique_thunk.h
+++ unique_thunk.h
@@ -2,33 +2,38 @@
 
 #include <functional>
 #include <memory>
+#include <thread>
+#include <future>
 
 namespace xlnagla{
 
-template<class T, class Del = std::default_delete<T> >
+	template<class T, std::launch lnch = std::launch::sync, class Del = std::default_delete<T> >
 class unique_thunk{
 private:
-std::function<T* ()> f;
+std::future<T*> f;
+//std::function<T* ()> f;
 std::unique_ptr<T, Del> t;
 bool evaluated;
 public:
-unique_thunk(std::function<T* ()> t):f(t),evaluated(false){}
-	const T& value() const 	{
-		//logically const - as far as the semantics are concerned, this is just a wrapper to unique_ptr.
-		unique_thunk<T>* l = const_cast<unique_thunk<T>*>(this);
-		if (!l->evaluated) {
-			l->t = std::unique_ptr<T>(std::move(l->f()));
-		} 
-		l->evaluated = true; 
-		return *t;
-	}
+	unique_thunk(std::function<T* ()> t):f(std::async(lnch, t)),evaluated(false){}
 
-	T& value() 	{
-		if (!evaluated) 
-			t = std::unique_ptr<T>(std::move(f())); 
-		evaluated = true; 
-		return *t;
-	}
+const T& value() const 	{
+	//logically const - as far as the semantics are concerned, this is just a wrapper to unique_ptr.
+	unique_thunk<T>* l = const_cast<unique_thunk<T>*>(this);
+	if (!l->evaluated) {
+		l->t = std::unique_ptr<T>(std::move(l->f.get()));
+	} 
+	l->evaluated = true; 
+	return *t;
+}
+
+T& value() 	{
+	if (!evaluated) 
+		t = std::unique_ptr<T>(std::move(f.get())); 
+	evaluated = true; 
+	return *t;
+}
+
 unique_thunk& operator= (unique_thunk&& _Right){
 	if (_Right.evaluated)
 		t = std::move(_Right.t);
@@ -39,8 +44,8 @@ unique_thunk& operator= (unique_thunk&& _Right){
 	evaluated = _Right.evaluated;
 	return (*this);
 }
-template<class Type2, class Del2>
-unique_thunk& operator= (unique_thunk<Type2, Del2>&& _Right){
+template<class Type2, std::launch lnch2, class Del2>
+unique_thunk& operator= (unique_thunk<Type2, lnch2, Del2>&& _Right){
 	if (_Right.evaluated)
 		t = std::move(_Right.t);
 	else{
